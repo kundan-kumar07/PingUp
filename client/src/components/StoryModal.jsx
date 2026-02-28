ce593f70935bec9a6ca8b7251a77e2b2d445bc20
kundan kumar <dubeykundan351@gmail.com>
2026-02-28 14:55:39 +0530

    feat:add create story and preview of it

---
+++ client/src/components/StoryModal.jsx
@@ -0,0 +1,84 @@
+import { ArrowLeft, TextIcon, Upload } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+
+const StoryModel = ({setShowModal,fetchStories}) => {
+  const bgColors = [
+    "#4f46e5","#6366f1","#3b82f6","#0ea5e9","#06b6d4",
+    "#10b981","#22c55e","#f59e0b","#f97316","#ef4444",
+    "#ec4899","#8b5cf6","#9333ea","#14b8a6"
+  ]
+
+  const [mode,setMode]=useState("text");
+  const [background,setBackground]=useState(bgColors[0]);
+  const [text,setText]=useState("");
+  const [media,setMedia]=useState(null);
+  const [previewUrl,setPreviewUrl]=useState(null);
+
+  const handleMediaUpload=(e)=>{
+    const file=e.target.files?.[0];
+    if(file){
+      setMedia(file);
+      setPreviewUrl(URL.createObjectURL(file));
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      if (previewUrl) {
+        URL.revokeObjectURL(previewUrl);
+      }
+    };
+  }, [previewUrl]);
+
+  const handleCreateStory=async ()=>{
+
+  }
+
+  return (
+    <div className='fixed inset-0 z-50 min-h-screen bg-black/80 backdrop-blur text-white flex items-center justify-center p-4'>
+      <div className='w-full  max-w-md'>
+        <div className='text-center mb-4 flex items-center justify-between'>
+          <button onClick={()=>setShowModal(false)}  className='text-white  p-2 cursor-pointer'>
+          <ArrowLeft/>
+        </button>
+        <h2 className='text-lg font-semibold'>Create Story</h2>
+        <span className='w-10'></span>
+
+        </div>
+        <div className='rounded-lg h-96 items-center justify-center relative' style={{backgroundColor:background}}>
+          {mode==='text' && (
+            <textarea name='' id='' className='bg-transparent text-white w-full h-full p-6 text-lg resize-noe focus:outline-none' placeholder="What's on your mind?" onChange={(e)=>setText(e.target.value)} value={text}></textarea>
+          )}
+          {mode==='media' && previewUrl &&(
+            media?.type.startsWith('image')?(
+              <img src={previewUrl} alt="" className='object-contain max-h-full' />
+            ):<video src={previewUrl} className='object-contain max-h-full' />
+
+          )
+          }
+
+
+        </div>
+        <div className='flex mt-4 gap-2'>
+          {bgColors.map((color)=>(
+            <button key={color} className='w-6 h-6 rounded-full ring cursor-pointer' style={{backgroundColor:color}} onClick={()=>setBackground(color)}/>
+          ))}
+
+        </div>
+        <div className='flex gap-2 mt-4'>
+          <button onClick={()=>{setMode('text'); setMedia(null); setPreviewUrl(null)}} className={`flex-1 flex items-center cursor-pointer justify-center gap-2 p-2 rounded ${mode==='text'?"bg-white text-black":"bg-zinc-800"}`}>
+            <TextIcon size={18}/ > Text
+          </button>
+          <label className={`flex-1 flex items-center justify-center gap-2 p-2 rounded cursor-pointer ${mode==='media' ? "bg-white text-black":"bg-zinc-800"}`}>
+            <input onChange={(e)=>{handleMediaUpload(e);setMode('media')}} type="file" name="" id="" accept='image/*,video/*'  className='hidden'/>
+            <Upload size={18}/>Photo/Video
+          </label>
+
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default StoryModel
